d0d98b553b1fa8d163ea6b804ffb2bd573dc016b
caitengwei <tengweicai@gmail.com>
2020-01-24 00:14:35 +0800

    ch5 practices

---
+++ src/gopl.io/ch5/sum.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func sum(vals ...int) int {
+	total := 0
+	for _, val := range vals {
+		total += val
+	}
+	return total
+}
+
+func main() {
+	fmt.Println(sum())           // "0"
+	fmt.Println(sum(3))          // "3"
+	fmt.Println(sum(1, 2, 3, 4)) // "10"
+
+	values := []int{ 4, 6, 8, 10 }
+	fmt.Println(sum(values...))
+}
