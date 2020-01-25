7fe8d5f14a0a5fe4d10cc72e5f837d4eeeeaccfc
caitengwei <tengweicai@gmail.com>
2020-01-26 00:56:09 +0800

    bank1

---
+++ ch9/bank/bank.go
@@ -0,0 +1,22 @@
+package bank
+
+var deposits = make(chan int) // send amount to deposit
+var balances = make(chan int) // receive balance
+
+func Deposit(amount int) { deposits <- amount }
+func Balance() int       { return <-balances }
+
+func teller() {
+	var balance int
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+		case balances <- balance:
+		}
+	}
+}
+
+func init() {
+	go teller()
+}
